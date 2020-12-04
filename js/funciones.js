8b7af74ab5690f45afbe54010a3921d1c2c53e38
unknown <varleo.95@gmail.com>
2020-12-04 11:14:04 -0500

    primera_entrada

---
+++ js/funciones.js
@@ -0,0 +1,14 @@
+var entro = 0;
+function ver(){
+if (entro == 0){
+ document.getElementById('Seccion_sesion').style.display="block";
+ entro = 1;
+}
+else{
+    document.getElementById('Seccion_sesion').style.display= "none";
+    entro=0;
+
+}
+
+};
+
