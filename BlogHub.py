3f8102223cfeefbddb4f51acec95e95803bc4320
unknown <varleo.95@gmail.com>
2020-12-05 13:17:22 -0500

    Cambio a Flask y Ruteo Basico

--- BlogHub.py
+++ BlogHub.py
@@ -11,9 +11,31 @@ def login():
 
 
 
+#ventana Registro
+@app.route('/registro')
+def registro():
+    return render_template('Vista_Registro.html')
 
 
 
+@app.route('/inicio')
+def inicio():
+    return render_template('inicio.html')
+
+
+
+@app.route('/perfil')
+def perfil():
+    return render_template('perfil.html')
+
+@app.route('/BlogPropio')
+def BlogPropio():
+    return render_template('Vista_Blog_Propio.html')
+
+
+@app.route('/BlogPublico')
+def BlogPublico():
+    return render_template('blog_publico.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
