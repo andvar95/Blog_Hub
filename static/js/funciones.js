b3aec35ee5747865ddf0e6f21d9f489997c89254
unknown <varleo.95@gmail.com>
2020-12-07 22:52:37 -0500

    Adicion de Logo

--- static/js/funciones.js
+++ static/js/funciones.js
@@ -1,18 +1,15 @@
 var entro = 0;
 
 function ver() {
+    
     if (entro == 0) {
-        document.getElementById('Seccion_sesion').style.display = "block";
+        document.getElementById('Seccion_sesion').style.visibility = "visible";
         entro = 1;
     } else {
-        document.getElementById('Seccion_sesion').style.display = "none";
+        document.getElementById('Seccion_sesion').style.visibility= "hidden";
         entro = 0;
 
     }
 
 };
 
-function quitar() {
-    document.getElementById('Seccion_sesion').style.display = "none";
-
-}
